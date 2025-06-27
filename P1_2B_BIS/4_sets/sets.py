63a861bc5f24ac39059f05cc7c0faf6be91d279a
Maximo Gamon <maximo_3141240179@utd.edu.mx>
2025-06-26 22:09:27 -0600

    Add files via upload

--- P1_2B_BIS/4_sets/sets.py
+++ P1_2B_BIS/4_sets/sets.py
@@ -1,5 +1,42 @@
 '''
+Es un tipo de dato para tener una coleccion de valores pero no tiene ni indice ni orden
+
 Set es una coleccion desordenada, inmutable* y no indexada.
 No hay miembros duplicados.
+
+
 '''
+import os
+os.system("cls")
+
+personas={"Ramiro","Choche","Lupe"}
+print(personas)
+personas.add("Choche")
+print(personas)
+personas.pop()
+print(personas)
+personas.clear()
+print(personas)
+
+varios={3.12,3,True,"Hola"}
+print(varios)
+
+
+#Ejemplo Crear un programa que solicite los emails de los alumnos de la UTD almacenar en una lista 
+# y posteriormente mostrar en pantalla los emails sin duplicados
+os.system("cls")
+opc="si"
+emails=[]
+while opc == "si":
+    emails.append(input("Dame el emial: "))
+
+    opc=input("¿Deseas solicitar otro email (si/no?)").lower
+
+#Impprimir los emails sin duplicados 
+
+print(emails)
+set1=set(emails)
+print(set1)
+emails=list(set1)
+print(emails)
 
