ab3b27c7f1817e4b0f8e1dca8ef26931e23c4412
Maximo Gamon <maximo_3141240179@utd.edu.mx>
2025-07-06 18:48:29 -0600

    Delete P1_2B_BIS/2_modulos_paquetes/utilizar_paquete.py

--- P1_2B_BIS/2_modulos_paquetes/utilizar_paquete.py
+++
@@ -1,7 +0,0 @@
-from paquete1 import modulos
-
-print(modulos.saludar("Daniel Contreras"))
-
-nom,tel=modulos.solicitarDatos2()
-print(f"'\n\t .::Agenda telefonica::. \n\t Nombre´ {nom} \n\t Telefono: {tel} ")
-modulos.espereTecla
