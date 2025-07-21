c7d78d13204f3bfd11586ecf10ae62bd14f7ff4a
Maximo Gamon <maximo_3141240179@utd.edu.mx>
2025-07-21 08:57:45 -0600

    Delete P3/simulacro1.py

--- P3/simulacro1.py
+++
@@ -1,9 +0,0 @@
-lista=[]
-if len(lista)==0:
-    resp="si"
-    while resp=="si":
-        lista.append(input("Dame la frase: ").upper())
-        resp=input("¿Desea solicitar otra frase? (si/no)").lower().strip()
-
-print(lista)
-       
